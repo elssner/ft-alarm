9ff30ad1710b79422e7174280a8e06fa6feab43e
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-15 15:49:16 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    while (true) {
+        while (!(input.buttonIsPressed(Button.B))) {
+            if (input.pinIsPressed(TouchPin.P3)) {
+                pins.digitalWritePin(DigitalPin.P2, 1)
+                pins.digitalWritePin(DigitalPin.C16, 1)
+                basic.pause(500)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+                pins.digitalWritePin(DigitalPin.C16, 0)
+                basic.pause(500)
+            }
+        }
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(5000)
+    }
 })
